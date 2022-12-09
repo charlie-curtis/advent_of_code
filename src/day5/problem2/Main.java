2c8db6f6d9b25a2577a99feec273dc0654b04c8d
Charlie Curtis <charlie-curtis@outlook.com>
2022-12-09 00:37:01 -0500

    do day5

---
+++ src/day5/problem2/Main.java
@@ -0,0 +1,75 @@
+package day5.problem2;
+
+import helpers.FileParser;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    FileParser parser = new FileParser("src/day5/input.txt");
+    BufferedReader br = parser.tryOpen();
+
+    List<Stack<Character>> holder = new ArrayList<>();
+
+
+    String string1 = "JHGMZNTF";
+    String string2 = "VWJ";
+    String string3 = "GVLJBTH";
+    String string4 = "BPJNCDVL";
+    String string5 = "FWSMPRG";
+    String string6 = "GHCFBNVM";
+    String string7 = "DHGMR";
+    String string8 = "HNMVZD";
+    String string9 = "GNFH";
+
+    ArrayList<String> strings = new ArrayList<>(List.of(string1, string2, string3, string4, string5, string6, string7,string8,string9));
+
+    for (int i = 0; i < 9; i++) {
+      Stack<Character> stack = new Stack<>();
+      String s = strings.get(i);
+      Arrays.stream(s.split("")).map(s1->s1.charAt(0)).forEach(stack::push);
+      holder.add(stack);
+    }
+
+    String line;
+
+    Stack<Character> temp = new Stack<>();
+
+    Pattern p = Pattern.compile("\\s(\\d+)");
+    while ((line = br.readLine()) != null) {
+      Matcher m = p.matcher(line);
+
+      m.find();
+      int moveCount = Integer.parseInt(line.substring(m.start(), m.end()).trim());
+      m.find();
+      int start = Integer.parseInt(line.substring(m.start(), m.end()).trim());
+      m.find();
+      int end = Integer.parseInt(line.substring(m.start(), m.end()).trim());
+
+      Stack<Character> from = holder.get(start-1);
+      Stack<Character> to = holder.get(end-1);
+
+      for (int i = 0; i < moveCount; i++) {
+        temp.push(from.pop());
+      }
+      for (int i = 0; i < moveCount; i++) {
+        to.push(temp.pop());
+      }
+    }
+
+
+    StringBuilder answer = new StringBuilder();
+    for (int i = 0; i < holder.size(); i++) {
+      answer.append(holder.get(i).peek());
+    }
+
+    System.out.println(answer);
+  }
+}
